5875d39d877278dc0d7b4aea3234cff2cf812181
wikimagi <67326896+wikimagi@users.noreply.github.com>
2020-06-23 16:12:48 +0000

    Create wikipedia-api.php

---
+++ wikipedia-api.php
@@ -0,0 +1,47 @@
+/*  Access Wikipedia API && Redirect to New URL with operator */
+
+$endPoint = "https://en.wikipedia.org/w/api.php";
+$params1 = [ "action" => "query",
+    "format" => "json",
+    "list" => "random",
+    "rnnamespace" => "0",
+    "rnlimit" => "45"
+];
+
+$random_url = $endPoint . "?" . http_build_query( $params1 );
+
+$ch = curl_init( $random_url );
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+$output = curl_exec( $ch );
+curl_close( $ch );
+
+$result = json_decode( $output, true );
+//var_dump( $result );
+
+
+foreach( $result["query"]["random"] as $k => $v ) {
+			$rand_art_title=$v["title"];
+				$title_wikified = str_replace(" ", "_", "$rand_art_title");
+			 	$url_encoded = urlencode("$title_wikify");
+				//$urlencoded = urlencode($rand_art_title);
+				//$title_decoded = str_replace(" ", "_", "$rand_art_title");
+				//$title_decoded = str_replace(".", "%2E", "$rand_art_title");
+   			      $url = "https://en.wikipedia.org/wiki/$title_wikified";
+			
+      
+      
+      $curlimage = exec("curl $url | grep og:image");
+			$trim_img_front = substr("$curlimage", 43);
+			$img_src = rtrim($trim_img_front, '"/>');
+						//$random_url = $endPoint . "?" . http_build_query( $params1 );
+						
+
+		if($img_src){
+					
+					//$img_url_encoded = urlencode("$img_src");
+					$img_url = str_replace("/", "support_wikipedia", "$img_src");
+		?>
+					<meta http-equiv=Refresh content='0; url=https://bigview.net/wikipedia/<? print "$title_wikified"; ?>/<? print "$img_url"; ?>' />
+			<?
+					die();	} } }
+			
